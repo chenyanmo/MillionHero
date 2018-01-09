030c211d81841d69e65b70a6dee6e69c4094a355
DESKTOP-A03Q91A\刘自豪 <1004657064@qq.com>
2018-01-09 19:00:06 +0800

    sxcdsv

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ data_directory = "screenshots"
 
 default_answer_number = 2
 
-hanwan_appcode = "714501eede0b4ac9a75a11af64b3b4d7"
+hanwan_appcode = "e669913d8c924381b2ef674912abaaa2"
 
 text_summary = True
 
